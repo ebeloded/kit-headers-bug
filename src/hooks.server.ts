6721be212e59feaa245df8e4a018163b7ece7987
Evgenij (Eugene) Beloded <belodedevgenij@gmail.com>
2023-01-10 08:52:07 +0700

    Add custom header to make it easier to spot

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,6 +2,7 @@ import type { Handle } from '@sveltejs/kit'
 
 export const handle: Handle = async ({ event, resolve }) => {
   event.setHeaders({
+    'x-foo': 'bar',
     'cache-control': 'no-store',
   })
   return resolve(event)
